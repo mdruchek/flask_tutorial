634643d94cf5620ff1618a8bd0984dedc2dc1283
Mikhail <mdruchek@yandex.ru>
2024-06-11 09:08:49 +0300

    fixed bugs

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -6,7 +6,7 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SEKRET_KEY='dev',
+        SECRET_KEY=b'dev',
         DATABASE=os.path.join(app.instance_path, 'flask.sqlite')
     )
 
